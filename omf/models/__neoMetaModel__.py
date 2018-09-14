75867393fcd10d9f1490d09764c99504a9034b42
ejtalbot <erikjamestalbot@gmail.com>
2018-09-14 12:10:36 -0400

    custom json function to handle bytes objects

--- omf/models/__neoMetaModel__.py
+++ omf/models/__neoMetaModel__.py
@@ -181,3 +181,9 @@ def roundSig(x, sig=3):
 def _test():
 	""" No test required for this file. """
 	pass
+
+def to_json(python_object):
+	if isinstance(python_object, bytes):
+		return {'__class__': 'bytes',
+				'__value__': list(python_object)}
+	raise TypeError(repr(python_object) + ' is not JSON serializable')
